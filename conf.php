9c1f089730c8e8e136814bd620c7f89e373cfe05
Walkxz <55284792+Walkxz@users.noreply.github.com>
2019-10-28 20:54:57 +0700

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,6 @@
+<?php
+const VK_KEY = "4fcdda2c358caaa6edb8eacec24514d39dc77fdd4967d2181126057c61275d3371ecb2664c83b8ac11467";
+const ACCESS_KEY = "3697cb62";
+const VERSION = "5.89";
+const BTN_1 = [["command" => 'btn_1'], "Свег есть?", "green"];
+const BTN_2 = [["command" => 'btn_2'], "SPAM!!", "red"];
